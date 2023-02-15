eb6f1eb76c63993ad883ed707b3796f956659496
Carlos Moraes <52669963+cgmoraes@users.noreply.github.com>
2023-02-15 14:38:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,25 +15,24 @@ ucs = Modeling()
 # Cria a classe Grade com os métodos que definem as rotas da API.
 class Grade():
 
-    # Rota "/disciplinas" - rota para obter as informações de todas as disciplinas, utilizando o método GET.
+    # Rota "/disciplinas" - rota para listar todas as disciplinas disponíveis, utilizando o método GET.
     @app.route("/disciplinas", methods=['GET'])
     def get():
         # Chama o método get_ucs da instância da classe Modeling e retorna a resposta em formato JSON.
         return ucs.get_ucs()
 
-    # Rota "/disciplinas" - rota para adicionar uma nova disciplina, utilizando o método POST.
+    # Rota "/disciplinas" - rota para retornar disciplinas disponíveis, utilizando o método POST.
     @app.route("/disciplinas", methods=['POST'])
     def post():
 
         # Obtém os dados do objeto JSON enviado na requisição POST.
         data = request.get_json()
 
-        # Chama o método uc_analizer da instância da classe Modeling e passa o objeto JSON com as informações da disciplina.
+        # Chama o método uc_analyzer da instância da classe Modeling e passa o objeto JSON com as informações das disciplinas.
         return ucs.uc_analyzer(data['items'])
 
 # Verifica se este arquivo é o arquivo principal que está sendo executado.
 if __name__ == '__main__':
     # Executa a aplicação Flask com o método run(). 
-    # O servidor é iniciado na porta especificada, que é a porta definida na variável de ambiente "PORT", ou a porta 8080 caso essa variável não esteja definida.
-    # O modo de depuração é ativado para facilitar o desenvolvimento e depuração da aplicação.
+    # O servidor é iniciado na porta 8080
     app.run(host="0.0.0.0", port=8080)
